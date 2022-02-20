f48fd3612c9e43554ccda12657175eb2c2c04ad9
Paul Standley <paulstandley1972@gmail.com~>
2022-02-20 14:45:17 +0000

    move init funtion to html

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,11 +62,3 @@
 				});
 
 })(jQuery);
-
-// Add Goggle map to dom
-function initMap() {
-	const map = new google.maps.Map(document.getElementById("map"), {
-  	zoom: 10,
-	center: { lat: 53.451832, lng: -2.080206 },
-	});
-}
