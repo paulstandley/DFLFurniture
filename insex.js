bb34de2fc7814f7224045085c1bf7256bf0d04a6
Paul Standley <paulstandley1972@gmail.com~>
2022-02-20 15:14:50 +0000

    move init to index js file

---
+++ insex.js
@@ -0,0 +1,8 @@
+
+// Add Goggle map to dom
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: { lat: 53.451832, lng: -2.080206 },
+    });
+}
