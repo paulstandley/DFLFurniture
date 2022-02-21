d8fc640c2487ef089b8904d1907b8f8b84348979
Paul Standley <paulstandley1972@gmail.com~>
2022-02-21 20:04:45 +0000

    add google map 2nd time lucky

--- insex.js
+++
@@ -1,8 +0,0 @@
-
-// Add Goggle map to dom
-function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: { lat: 53.451832, lng: -2.080206 },
-    });
-}
